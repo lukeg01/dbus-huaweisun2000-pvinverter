73ba76f1ec30d7f0bee0bf8fe877d74a8eebe77c
Luke <luke.geelen@gmail.com>
2026-05-10 16:36:23 +0200

    update install sequence to own branch

--- dbus-huaweisun2000-pvinverter/setup/install_or_update.sh
+++ dbus-huaweisun2000-pvinverter/setup/install_or_update.sh
@@ -6,10 +6,10 @@ TMP_FILE="/tmp/dbus-huaweisun2000-pvinverter.zip"
 
 if [ "$1" == "dev" ] ; then
     # head of main branch
-    URL="https://github.com/kcbam/dbus-huaweisun2000-pvinverter/archive/refs/heads/main.zip"
+    URL="https://github.com/lukeg01/dbus-huaweisun2000-pvinverter/archive/refs/heads/main.zip"
 else
     # latest release
-    URL="https://github.com/kcbam/dbus-huaweisun2000-pvinverter/releases/latest/download/project.zip"
+    URL="https://github.com/lukeg01/dbus-huaweisun2000-pvinverter/releases/latest/download/project.zip"
 fi
 
 rm -f ${TMP_FILE}
@@ -20,10 +20,14 @@ wget -q -O ${TMP_FILE} ${URL}
 if [ "$1" == "dev" ] ; then
     unzip -o ${TMP_FILE} -d /tmp
     rm -rf /tmp/dbus-huaweisun2000-pvinverter-main/.git*
-    cp -a /tmp/dbus-huaweisun2000-pvinverter-main/* /data/dbus-huaweisun2000-pvinverter/
+    # Copy from the driver subdirectory within the extracted archive
+    cp -a /tmp/dbus-huaweisun2000-pvinverter-main/dbus-huaweisun2000-pvinverter/* /data/dbus-huaweisun2000-pvinverter/
     rm -rf /tmp/dbus-huaweisun2000-pvinverter-main
 else
-    unzip -o ${TMP_FILE} -d /data/dbus-huaweisun2000-pvinverter
+    # Extract and copy from the driver subdirectory
+    unzip -o ${TMP_FILE} -d /tmp
+    cp -a /tmp/dbus-huaweisun2000-pvinverter/dbus-huaweisun2000-pvinverter/* /data/dbus-huaweisun2000-pvinverter/
+    rm -rf /tmp/dbus-huaweisun2000-pvinverter
 fi
 rm -f ${TMP_FILE}
 
